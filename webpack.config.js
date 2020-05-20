a947efafd8d4f1388c7acfeaacf41d55c7501087
irfiacre <firaduk@yahoo.com>
2020-05-20 14:18:47 +0200

    bg(fixing production link): add fixing heroku link - add a simple server to run our app. - add a modified webpack.
    [ finishes  #172933075]

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 var DIST_DIR = path.resolve(__dirname, 'dist'); // Define the distribution directory
 var SRC_DIR = path.resolve(__dirname, 'src'); // Define the source directory
-
 /**
  * HTML Webpack Plugin configuration
  */
@@ -12,31 +12,75 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
   filename: 'index.html',
   inject: 'body',
 });
+/* Webpack configuration setup     
+*/
 
-/**
- * Webpack configuration setup
- */
-var config = {
-  entry: SRC_DIR + '/index.js',
-  output: {
-    path: DIST_DIR + '/app',
-    filename: 'index.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css/,
-        use: ['style-loader','css-loader' , 'sass-loader'],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        include: SRC_DIR,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins: [htmlWebpackPlugin],
-};
+module.exports =(env)=> {
+    const isProduction = env === 'production';
+    return{
+        entry: SRC_DIR + '/index.js',
+        output: {
+          path: DIST_DIR,
+          filename: 'index.bundle.js',
+          publicPath: '/',
+        },
+        module: {
+          rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                     {
+                       loader: MiniCssExtractPlugin.loader,
+                     },
+                     {
+                         loader: "css-loader",
+                         options: {
+                             sourceMap: true,
+                         }
+                     },
+                     {
+                         loader: "sass-loader",
+                         options:{
+                             sourceMap: true,
+                         }
+                     }
 
-module.exports = config;
+                     ],
+              },
+              {
+                test: /\.(js|jsx)$/,
+                include: SRC_DIR,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+              },
+              {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {},
+                  },
+                ],
+              },
+              {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              },
+            ],
+          },
+    
+          devtool: isProduction ? 'source-map':'cheap-module-eval-source-map',
+    
+          devServer: {
+            historyApiFallback: true,
+          },
+          plugins: [
+              htmlWebpackPlugin,
+              new MiniCssExtractPlugin, 
+            ],
+    }
+    };
