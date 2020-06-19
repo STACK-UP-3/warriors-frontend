de98f5dee0d275f5b250d1e39e60bb72b760d419
Tony Stark <fordarnold@gmail.com>
2020-06-19 16:37:42 +0200

    bug(auth): fix error on redirection after login
    [Finishes #173419083]

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,9 @@
-import React,{ useEffect } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import M from 'materialize-css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { onUserLogin,TokenAuth } from '../redux/actions/loginActions';
+import { onUserLogin, TokenAuth } from '../redux/actions/loginActions';
 import LoginForm from './loginForm';
 import Footer from './footer';
 import Message from './requestFail';
@@ -12,86 +12,86 @@ import SocialAuth from './socialAuth';
 
 toast.configure();
 
-export default ()=>{
+export default () => {
+  const loginStore = useSelector((store) => store.loginData);
+  const dispatch = useDispatch();
 
-    const loginStore = useSelector( store=> store.loginData );
-    const dispatch = useDispatch();
+  useEffect(() => {
+    M.AutoInit();
 
-    useEffect( ()=>{
-        M.AutoInit();
-        if(loginStore.error){
-            toast.error( <Message />,{
-                position: toast.POSITION.TOP_CENTER,
-                toastId: 'loginFetchError',
-            });
-        }
-        
-        if(loginStore.userInfo.status === 200){
-            const token = JSON.stringify(loginStore.userInfo.data.access_token);
-            localStorage.setItem('token', token);
-            window.location.assign('/dashboard');
-        } 
+    console.log('<----->');
+    if (loginStore.error) {
+      toast.error(<Message />, {
+        position: toast.POSITION.TOP_CENTER,
+        toastId: 'loginFetchError',
+      });
+    }
 
-        let token;
-        const url  = window.location.search;
-        const arr = url.split(""); 
-        arr.splice(0,7); 
-        
-        if(arr.length){
-            token = arr.join("")
-        }
+    if (loginStore.userInfo.status === 200) {
+      const token = JSON.stringify(loginStore.userInfo.data.access_token);
+      localStorage.setItem('token', token);
+      return window.location.assign('/dashboard');
+    }
 
-        if(localStorage.getItem("token")){
-            token = localStorage.getItem("token");
-        }
-        
+    let token;
 
-        if(token) return tokenAuthenticator(token)
-    },[]);
-    
-    const tokenAuthenticator = (token) => { dispatch(TokenAuth(token)); }
-    const onFormSubmit = data => { dispatch(onUserLogin(data));  }
+    // const url = window.location.search;
+    // const arr = url.split('');
 
-    const loginsideContainerTitle = 'Welcome Back to BareFoot Nomad';
-    const loginsideContainerSubtitle  = (
-            <div>
-                <p>
-                     If you have not created an account please sign up to use our services
-                </p>
-                    <br />
+    // arr.splice(0, 7);
 
-                <h4
-                    onClick={ ()=> window.location.assign('/signup') }
-                >
-                    sign up
-                </h4>
-            </div>
-    );
-    
-    return (
-        <div className='loginContainer'>
-        <div className='main'>
-            <SideContainer title={ loginsideContainerTitle } subtitle ={ loginsideContainerSubtitle  }/>
-                
-                <div className='formContainer'>
+    // if (arr.length) {
+    //   token = arr.join('');
+    // }
 
-                    <h1> Sign in to BareFoot Nomad </h1>
+    token = localStorage.getItem('token');
 
-                        <LoginForm 
-                            onSubmit={ onFormSubmit } 
-                            userInfo = { loginStore.userInfo } 
-                            isLoading = {loginStore.loading}
-                            testdata='loginForm' 
-                        />
-                            <br/>
-                        <SocialAuth />
-                    
-                </div>
-        </div>
+    if (token) return window.location.assign('/dashboard');
+  });
 
-        <Footer />
+  // const tokenAuthenticator = (token) => {
+  //   dispatch(TokenAuth(token));
+  // };
 
+  const onFormSubmit = (data) => {
+    dispatch(onUserLogin(data));
+  };
 
+  const loginsideContainerTitle = 'Welcome Back to BareFoot Nomad';
+  const loginsideContainerSubtitle = (
+    <div>
+      <p>
+        If you have not created an account please sign up to use our services
+      </p>
+      <br />
+
+      <h4 onClick={() => window.location.assign('/signup')}>sign up</h4>
     </div>
-    );
-}
+  );
+
+  return (
+    <div className="loginContainer">
+      <div className="main">
+        <SideContainer
+          title={loginsideContainerTitle}
+          subtitle={loginsideContainerSubtitle}
+        />
+
+        <div className="formContainer">
+          <h1> Sign in to BareFoot Nomad </h1>
+
+          <LoginForm
+            onSubmit={onFormSubmit}
+            userInfo={loginStore.userInfo}
+            isLoading={loginStore.loading}
+            testdata="loginForm"
+          />
+          <br />
+          <SocialAuth />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
