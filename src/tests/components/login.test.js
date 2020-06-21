a0da43bc17819bea9fc7a7dc506274e27e59d000
Mwumva <fordarnold@gmail.com>
2020-06-21 20:21:45 +0200

    bug(auth): fix error on redirection after login (#13)

--- src/tests/components/login.test.js
+++ src/tests/components/login.test.js
@@ -6,128 +6,143 @@ import thunk from 'redux-thunk';
 import Login from '../../components/login';
 import configureStore from '../../redux/configureStore';
 import LoginForm from '../../components/loginForm';
-import {tokenData} from '../fixtures/login.testData';
+import { tokenData } from '../fixtures/login.testData';
 
-describe('*************** Testing the Login  component ***************',()=>{
+describe('*************** Testing the Login  component ***************', () => {
+  const store = configureStore();
+  let onFormSubmit;
+  const { location } = window;
 
-    const store = configureStore();
-    let onFormSubmit;
-    const { location } = window;
+  beforeEach(() => {
+    onFormSubmit = jest.fn();
+    delete window.location;
+    localStorage.removeItem('token');
+    window.location = { assign: jest.fn(), search: '' };
+  });
 
-    beforeEach(()=>{
-        onFormSubmit = jest.fn();
-        delete window.location;
-        localStorage.removeItem("token");
-		window.location = { assign: jest.fn(),search:'' };
+  afterAll(() => {
+    window.location = location;
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Should render the login page correctly', () => {
+    window.location = { assign: jest.fn(), search: `?token=${tokenData}` };
+    const store = mockStore([thunk])({
+      loginData: {
+        loading: false,
+        userInfo: {
+          status: 200,
+          message: 'You have signed in successfully',
+          data: {
+            access_token: tokenData,
+          },
+        },
+        error: null,
+      },
     });
 
-    afterAll(() => {
-		window.location = location;
-        expect(localStorage.getItem).toHaveBeenCalled()
-    });
-    
-    it('Should render the login page correctly', ()=>{
-        window.location = { assign: jest.fn(), search : `?token=${tokenData}` };
-        const store = mockStore([thunk])({
-            loginData: {
-                loading: false,
-                userInfo: {
-                    status: 200,
-                    message: 'You have signed in successfully',
-                    data:{
-                       access_token: tokenData,
-                    }
-                },
-                error: null
-            }, 
-        });
+    const wrapper = mount(
+      <Provider store={store}>
+        <Login />
+      </Provider>,
+    );
 
-        const  wrapper = mount(
-            <Provider store={ store }> 
-                <Login />
-            </Provider> 
-        );
+    const handleSubmit = jest.spyOn(
+      wrapper.find('[testdata="loginForm"]').props(),
+      'onSubmit',
+    );
+    wrapper.find('[testdata="loginForm"]').props().onSubmit();
+    expect(wrapper).toMatchSnapshot();
+    expect(handleSubmit).toHaveBeenLastCalledWith();
+  });
 
-        const handleSubmit = jest.spyOn(wrapper.find('[testdata="loginForm"]').props(), 'onSubmit');
-        wrapper.find('[testdata="loginForm"]').props().onSubmit();
-        expect(wrapper).toMatchSnapshot();
-        expect(handleSubmit).toHaveBeenLastCalledWith();
-    });
+  it('should submit upon form call', () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = mount(
+      <Provider store={store}>
+        <LoginForm
+          onSubmit={onSubmitSpy}
+          userInfo={{ message: 'Test message' }}
+        />
+      </Provider>,
+    );
 
-    it('should submit upon form call',() => {
-       const onSubmitSpy = jest.fn();
-       const wrapper = mount(
-            <Provider store={ store }> 
-                <LoginForm  onSubmit={ onSubmitSpy } userInfo={ { message: 'Test message' } }/>
-            </Provider> 
-        );
+    expect(wrapper).toMatchSnapshot();
 
-        expect(wrapper).toMatchSnapshot();
+    wrapper
+      .find('input')
+      .at(0)
+      .simulate('change', {
+        target: { id: 'email', value: 'xxx@yyy.zzz' },
+      });
 
-        wrapper.find('input').at(0).simulate('change',{ 
-                target: { id: 'email', value: 'xxx@yyy.zzz'} 
-               });
-               
-       wrapper.find('input').at(1).simulate('change',{ 
-                target: { id: 'password', value: 'kkkkkkkk'} 
-               });
+    wrapper
+      .find('input')
+      .at(1)
+      .simulate('change', {
+        target: { id: 'password', value: 'kkkkkkkk' },
+      });
 
-            wrapper.find('form').simulate('submit',{
-                preventDefault: ()=>{},
-            });
-
-        expect(onSubmitSpy).toBeCalled();
+    wrapper.find('form').simulate('submit', {
+      preventDefault: () => {},
     });
 
-    it('Should test form inputs on change and forgot password',() => {
-        const testState = { email: 'aaa@bbbb.ccc', password:'jjjjjjj' }
-        const wrapper = mount(
-             <Provider store={ store }> 
-                 <LoginForm  
-                  email={ testState.email }
-                  password={ testState.password }
-                  history = { { push: path => path } }
-                  userInfo={ { message: 'Test message' } }
-                  />
-             </Provider> 
-         );
+    expect(onSubmitSpy).toBeCalled();
+  });
 
-        wrapper.find('input').at(0).simulate('change',{ 
-                 target: { id: 'email', value: 'xxx@yyy.zzz'} 
-                });
-                
-        wrapper.find('input').at(1).simulate('change',{ 
-                 target: { id: 'password', value: 'kkkkkkkk'} 
-                });
+  it('Should test form inputs on change and forgot password', () => {
+    const testState = { email: 'aaa@bbbb.ccc', password: 'jjjjjjj' };
+    const wrapper = mount(
+      <Provider store={store}>
+        <LoginForm
+          email={testState.email}
+          password={testState.password}
+          history={{ push: (path) => path }}
+          userInfo={{ message: 'Test message' }}
+        />
+      </Provider>,
+    );
 
-        wrapper.find('span').at(0).simulate('click');
+    wrapper
+      .find('input')
+      .at(0)
+      .simulate('change', {
+        target: { id: 'email', value: 'xxx@yyy.zzz' },
+      });
 
-        expect(window.location.assign).toHaveBeenCalled();
-        
-     })
+    wrapper
+      .find('input')
+      .at(1)
+      .simulate('change', {
+        target: { id: 'password', value: 'kkkkkkkk' },
+      });
 
-     it('Should test if Fetch does not work',() => {
-        const store = mockStore([thunk])({
-            loginData: {
-                loading: true,
-                userInfo: { },
-                error: 'TypeError: Failed to fetch',
-            }, 
-        });
+    wrapper.find('span').at(0).simulate('click');
 
-        const wrapper = mount(
-             <Provider store={ store }> 
-                 <Login />
-             </Provider> 
-         );
+    expect(window.location.assign).toHaveBeenCalled();
+  });
 
-         wrapper.find('h4').simulate('click');
+  it('Should test if Fetch does not work', () => {
+    const store = mockStore([thunk])({
+      loginData: {
+        loading: true,
+        userInfo: {},
+        error: 'TypeError: Failed to fetch',
+      },
+    });
 
-         wrapper.find('form').simulate('submit',{
-            preventDefault: ()=>{},
-        });
+    const wrapper = mount(
+      <Provider store={store}>
+        <Login />
+      </Provider>,
+    );
 
-        expect(window.location.assign).toHaveBeenCalled();
-        
-     })
+    wrapper.find('h4').simulate('click');
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault: () => {},
+    });
+
+    expect(window.location.assign).toHaveBeenCalled();
+  });
 });
